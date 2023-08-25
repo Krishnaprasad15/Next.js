2d9adf566493b75f2356bb169a92414bf7bc68a0
krishna1500 <krishna16151@gmail.com>
2023-08-25 12:30:47 +0530

    Dynamic routes

---
+++ pages/product/index.js
@@ -0,0 +1,18 @@
+
+const index = () => {
+  return (
+    <>
+    <h1>
+      Product 1
+    </h1>
+    <h1>
+      Product 2
+    </h1>
+    <h1>
+      Product 3
+    </h1>
+    </>
+  )
+}
+
+export default index
